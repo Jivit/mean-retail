27109518b1ed1b16488e991260be237ffef5c378
Grégory D'Angelo <dangelo.gregory@gmail.com>
2016-03-11 10:23:53 +0100

    Handle currency conversion

--- product.js
+++ product.js
@@ -1,15 +1,36 @@
 var mongoose = require('mongoose');
 var Category = require('./category');
+var fx = require('./fx');
 
 var productSchema = {
   name: { type: String, required: true },
   // Pictures must start with "http://"
   pictures: [{ type: String, match: /^http:\/\//i, require: true }],
   price: {
-    amount: { type: Number, required: true },
-    currency: { type: String, enum: ['USD', 'EUR', 'GBP'], required: true }
+    amount: {
+      type: Number,
+      required: true,
+      set: function(v){
+        this.internal.approximatePriceUSD =
+          v * (fx()[this.price.currency] || 1);
+        return v;
+      }
+    },
+    currency: {
+      type: String,
+      enum: ['USD', 'EUR', 'GBP'],
+      required: true,
+      set: function(v){
+        this.internal.approximatePriceUSD =
+          this.price.amount * (fx()[v] || 1);
+        return v;
+      }
+    }
   },
-  category: Category.categorySchema
+  category: Category.categorySchema,
+  internal: {
+    approximatePriceUSD: Number
+  }
 }
 
 var schema = new mongoose.Schema(productSchema);

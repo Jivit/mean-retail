176ffdd466f972c5ec6f4bc9ea0335333cdf2e3e
Grégory D'Angelo <dangelo.gregory@gmail.com>
2016-03-15 17:45:30 +0100

    Set up the Angular app

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.set('port', process.env.PORT || 3000);
 
 wagner.invoke(require('./config/auth'), { app: app });
 
+app.use(express.static(__dirname + "/public"));
+
 app.use('/api/v1', require('./app/api')(wagner));
+
+// frontend routes
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html');
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
